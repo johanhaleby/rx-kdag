1b694d1155caf6144da5d444bfa1de988c3d0999
Johan Haleby <johan.haleby@gmail.com>
2014-02-14 11:21:31 +0100

    Initial commit

---
+++ src/test/java/com/jayway/kdag/rx/AppTest.java
@@ -0,0 +1,40 @@
+package com.jayway.kdag.rx;
+
+import com.jayway.restassured.path.xml.XmlPath;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static com.jayway.restassured.path.xml.XmlPath.CompatibilityMode.HTML;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest  {
+    
+    @Test public void
+    file_crave_parsing() {
+        // Given
+        InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("filmcrave.html");
+
+        // When
+        List<String> topList = new XmlPath(HTML, resourceAsStream).getList("**.findAll { it.a.@href.text().startsWith('movie') }", String.class);
+
+        // Then
+        assertThat(topList).hasSize(100);
+    }
+
+    @Test public void
+    imdb_parsing() {
+        // Given
+        InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("imdb.html");
+
+        // When
+        List<String> topList = new XmlPath(HTML, resourceAsStream).getList("**.findAll { it.a.@href.text().startsWith('/title/')  }.a*.toString().findAll { !it.isEmpty() && it != 'X' }", String.class);
+
+        // Then
+        assertThat(topList).hasSize(250);
+    }
+}
