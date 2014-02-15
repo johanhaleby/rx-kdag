3a73a73c108d0e39801e50679a7a25bdcac4c307
Johan Haleby <johan.haleby@gmail.com>
2014-02-15 09:42:57 +0100

    Converted SentimentAnalysis to use infinite streams

--- src/main/java/com/jayway/kdag/rx/SentimentAnalysisExample.java
+++ src/main/java/com/jayway/kdag/rx/SentimentAnalysisExample.java
@@ -3,9 +3,15 @@ package com.jayway.kdag.rx;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.subscriptions.Subscriptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import static java.util.Arrays.asList;
 
@@ -16,7 +22,7 @@ public class SentimentAnalysisExample {
     public static void main(String[] args) throws IOException {
         List<String> sentences = IOUtils.readLines(Thread.currentThread().getContextClassLoader().getResourceAsStream("sentences.txt"));
 
-        Observable.from(sentences).
+        Observable.create(new InfiniteSentenceGenerator(sentences)).
                 map(String::toLowerCase).
                 map(sentence -> new ImmutablePair<>(sentence, analyze(POSITIVE, sentence) - analyze(NEGATIVE, sentence))).
                 doOnNext(pair -> {
@@ -31,7 +37,43 @@ public class SentimentAnalysisExample {
 
     }
 
+    private static List<String> shuffle(List<String> sentences) {
+        List<String> mutable = new ArrayList<>(sentences);
+        Collections.shuffle(mutable);
+        return Collections.unmodifiableList(mutable);
+    }
+
     private static long analyze(List<String> match, String sentence) {
         return match.stream().filter(sentence::contains).count();
+    }
+
+    private static class InfiniteSentenceGenerator implements Observable.OnSubscribeFunc<String> {
+        private final List<String> sentences;
+        private volatile boolean isRunning;
+
+        public InfiniteSentenceGenerator(List<String> sentences) {
+            this.sentences = sentences;
+            isRunning = true;
+        }
+
+        public void stop() {
+            isRunning = false;
+        }
+
+        @Override
+        public Subscription onSubscribe(Observer<? super String> observer) {
+            while (isRunning) {
+                for (String sentence : shuffle(sentences)) {
+                    int i = new Random().nextInt(500);
+                    try {
+                        Thread.sleep(i);
+                    } catch (InterruptedException e) {
+                        observer.onError(e);
+                    }
+                    observer.onNext(sentence);
+                }
+            }
+            return Subscriptions.empty();
+        }
     }
 }
